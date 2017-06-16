5fd9e8c1246bfcd3c643aa8e1edb303c654e2631
Marco Di Bari <m.dibari@bopen.eu>
2017-06-16 10:11:02 +0200

    New definition of p_result_type function to resolve Segfault error that arose dereferencing char pointer

--- mpy/metview.h
+++ mpy/metview.h
@@ -1,12 +1,12 @@
 
-void mp_init(int argc, char **argv);
+int mp_init(int argc, char **argv);
 void p_init();
 const char* p_hello_world(int argc);
 const char* p_call_function(const char* name, int arity);
 void p_push_number(int n);
 void p_push_string(const char *str);
 void p_push_request(void *req);
-char *p_result_type(void);
+int p_result_type(void);
 char *p_result_as_string(void);
 char *p_result_as_grib_path(void);
 double p_result_as_number(void);
