290e6d96a87292c84765206e31529030fbcd8a16
Marco Di Bari <m.dibari@bopen.eu>
2017-06-21 10:38:00 +0200

    Added new tests. Some code lines having function of testing have been removed from metview.py

--- mpy/metview.py
+++ mpy/metview.py
@@ -68,10 +68,6 @@ def make(name):
 lib.p_init()
 
 
-lib.p_push_number(5)
-lib.p_push_number(4)
-
-
 pr       = make('print')
 low      = make('lowercase')
 ds       = make('describe')
@@ -82,26 +78,6 @@ read     = make('read')
 
 
 ####################### User Program ###############
-
-# call Metview's 'print' function with any number of arguments
-pr('Start ', 7, 1, 3, ' Finished!')
-pr(6, 2, ' Middle ', 6)
-
-# call Metview's 'lowercase' string function
-a = low('MetViEw')
-print('output LOWERCASE function: %s\n' % a)
-
-
-gg = read({'SOURCE' : 'test.grib', 'GRID' : '80'})
-regidded_grib = shutil.copyfile(gg, 'test_gg_grid.grib')
-grib_path = read(regidded_grib)
-print('Regridded grib file path: %s\n' % grib_path)
-
-lib.p_push_grib(grib_path.encode('utf-8'))
-lib.p_call_function('plot'.encode('utf-8'), 1)
-
-
-
 
 # perform a MARS retrieval
 # - defined a request
--- tests/test_metview.py
+++ tests/test_metview.py
@@ -1,6 +1,35 @@
+import os
+import shutil
 
-import mpy.metview
+from mpy._metview import ffi, lib
+from mpy.metview import make
 
+
+def test_push_number():
+    lib.p_push_number(5)
+    lib.p_push_number(4)
+
+def test_print():
+    # call Metview's 'print' function with any number of arguments
+    pr = make('print')
+    pr('Start ', 7, 1, 3, ' Finished!')
+    pr(6, 2, ' Middle ', 6)
+
+def test_lowercase():
+    # call Metview's 'lowercase' string function
+    low = make('lowercase')
+    a = low('MetViEw')
+    assert a == 'metview'
 
 def test_read():
-    fieldset = metview.read('test.grib')
+    read = make('read')
+    path = os.path.dirname(__file__)
+    gg = read({'SOURCE' : os.path.join(path, 'test.grib'), 'GRID' : '80'})
+    regidded_grib = shutil.copyfile(gg, os.path.join(path, 'test_gg_grid.grib'))
+    grib_path = read(regidded_grib)
+    
+def test_plot():
+    grib_path = os.path.join(os.path.dirname(__file__), 'test_gg_grid.grib')
+    lib.p_push_grib(grib_path.encode('utf-8'))
+    lib.p_call_function('plot'.encode('utf-8'), 1)
+    os.remove(grib_path)
