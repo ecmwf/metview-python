8bb628c0454ea646fbf930d0e3334c30e534e36d
Alessandro Amici <alexamici@gmail.com>
2017-07-27 21:10:21 +0100

    Allow matplotlib.

--- docker_image/system-sync.sh
+++ docker_image/system-sync.sh
@@ -4,10 +4,11 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get -y upgrade
 apt-get -y --no-install-recommends install sudo vim curl
-apt-get -y --no-install-recommends install build-essential python3.5-dev python3-pip python3-venv
+apt-get -y --no-install-recommends install build-essential
+apt-get -y --no-install-recommends install python3.5-dev python3-pip python3-venv python3-tk
 apt-get -y build-dep metview
 apt-get -y build-dep magics++
 apt-get -y build-dep emoslib
-apt-get -y --no-install-recommends install liblapack-dev libncurses-dev libqt4-dev git x11-utils libffi-dev
+apt-get -y --no-install-recommends install liblapack-dev libncurses-dev libqt4-dev git x11-utils
 
 python3 -m venv cpython3
