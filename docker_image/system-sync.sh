5d689bcfc2e4c08c2d69bb62d360e90e4a9cd0df
Alessandro Amici <alexamici@gmail.com>
2017-07-31 10:48:26 +0200

    Add required dependency.

--- docker_image/system-sync.sh
+++ docker_image/system-sync.sh
@@ -9,6 +9,6 @@ apt-get -y --no-install-recommends install python3.5-dev python3-pip python3-ven
 apt-get -y build-dep metview
 apt-get -y build-dep magics++
 apt-get -y build-dep emoslib
-apt-get -y --no-install-recommends install liblapack-dev libncurses-dev libqt4-dev git x11-utils
+apt-get -y --no-install-recommends install liblapack-dev libncurses-dev libqt4-dev git x11-utils xterm
 
 python3 -m venv cpython3
