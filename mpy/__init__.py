7136ae7ca125c26ef0fbbe3183c26295252dbbe1
Alessandro Amici <a.amici@bopen.eu>
2018-01-25 21:41:46 +0100

    Exclude functions matching a python keyword ('in') and report the excluded functions for debugging.

--- mpy/__init__.py
+++ mpy/__init__.py
@@ -1,8 +1,12 @@
 
+import keyword
+
 from . import metview
 
 
 # Add to the module namespace all metview functions except operators like: +, &, etc.
 for func in metview.make('dictionary')():
-    if func.isidentifier():
+    if func.isidentifier() and not keyword.iskeyword(func):
         globals()[func] = metview.make(func)
+    else:
+        print('excluding %r' % func)
