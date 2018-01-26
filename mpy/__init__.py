8efa5bd3da4b2d869ce700f5abb2d6633498e2d9
Alessandro Amici <a.amici@bopen.eu>
2018-01-26 09:02:32 +0100

    Append a '_' to metview functions that clash with python reserved keywords as they cannot be used as identifiers, for example: 'in' -> 'in_'

--- mpy/__init__.py
+++ mpy/__init__.py
@@ -5,8 +5,13 @@ from . import metview
 
 
 # Add to the module namespace all metview functions except operators like: +, &, etc.
-for func in metview.make('dictionary')():
-    if func.isidentifier() and not keyword.iskeyword(func):
-        globals()[func] = metview.make(func)
+for metview_name in metview.make('dictionary')():
+    if metview_name.isidentifier():
+        python_name = metview_name
+        # NOTE: we append a '_' to metview functions that clash with python reserved keywords
+        #   as they cannot be used as identifiers, for example: 'in' -> 'in_'
+        if keyword.iskeyword(metview_name):
+            python_name += '_'
+        globals()[python_name] = metview.make(metview_name)
     else:
-        print('excluding %r' % func)
+        print('metview function %r not bound to python' % metview_name)
