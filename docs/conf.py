576d070144817846a10514ec6bfbe49d2eae7e6e
Alessandro Amici <a.amici@bopen.eu>
2018-03-17 18:44:14 +0100

    Cleanup the config of the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import mpy
-
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
