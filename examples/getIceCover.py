fe624c75cd78a2d6cec71aa246ea836ebff51b90
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2018-06-09 22:49:08 +0200

    add scatter plot fo ice and co2

--- examples/getIceCover.py
+++ examples/getIceCover.py
@@ -3,6 +3,8 @@ from ecmwfapi import ECMWFDataServer
 from cdo import Cdo
 from multiprocessing import Pool
 from tarfile import TarFile
+import pandas
+import matplotlib.pyplot as plt
     
 server = ECMWFDataServer()
 cdo    = Cdo()
@@ -48,6 +50,13 @@ if ( not os.path.exists(iceCover_file) ):
      })
 else:
     print("use existing file '%s'"%(iceCover_file))
+# compute the nh ice extent and the other time series
+iceExtent = 'ice_extent_%s-%s-daymean-SeptMin.nc'%(startYear,endYear)
+cdo.setattribute('sea_ice_extent@unit=m2,sea_ice_extent@standard_name=sea_ice_extent',
+         input  = '-setname,sea_ice_extent -yearmin -fldsum -mul -selmon,9 %s -gridarea %s'%(iceCover_file,iceCover_file),
+         output = iceExtent,force=False,
+         options = '-f nc')
+iceExtent_ds = cdo.readXDataset(iceExtent)
 
 # cams return tarballs of netcdf files
 co2_tarball = "co2_totalColumn.tar"
@@ -75,9 +84,11 @@ pool.close()
 pool.join()
 for k,v in co2_results.items():
     co2_results[k] = v.get()
-co2_timeSeries = cdo.yearmean(input = '-cat %s'%(' '.join([co2_results[x] for x in co2_files])),
-        output = 'co2_timeseries.nc', forceOutput=False,
+co2_timeSeries = 'co2_timeseries.nc'
+cdo.yearmean(input = '-cat %s'%(' '.join([co2_results[x] for x in co2_files])),
+        output = co2_timeSeries, force=False,
         options = '-f nc')
+co2_ds = cdo.readXDataset(co2_timeSeries)
 
 meth_tarball = "meth_totalColumn.tar"
 if ( not os.path.exists(meth_tarball) ):
@@ -103,14 +114,17 @@ pool.close()
 pool.join()
 for k,v in meth_results.items():
     meth_results[k] = v.get()
-meth_timeSeries = cdo.yearmean(input = '-cat %s'%(' '.join([meth_results[x] for x in meth_files])),
-        output = 'meth_timeseries.nc', forceOutput=False,
+meth_timeSeries = 'meth_timeseries.nc'
+cdo.yearmean(input = '-cat %s'%(' '.join([meth_results[x] for x in meth_files])),
+        output = meth_timeSeries, force=False,
         options = '-f nc')
+meth_ds = cdo.readXDataset(meth_timeSeries)
 # }}} ==========================================================================
-
-# compute the nh ice extent and the other time series
-iceExtent = 'ice_extent_1980-2014-daymean-SeptMin.nc'
-cdo.setattribute('sea_ice_extent@unit=m2,sea_ice_extent@standard_name=sea_ice_extent',
-         input  = '-setname,sea_ice_extent -yearmin -fldsum -mul -selmon,9 %s -gridarea %s'%(iceCover_file,iceCover_file),
-         output = iceExtent,force=False,
-         options = '-f nc')
+# scatter plot {{{ =============================================================
+iceExtent_ds.info()
+co2_ds.info()
+plt.scatter( co2_ds.sel(time=slice('1980-01-01', '2014-01-01')).to_array()[1,:,0,0,0],
+        iceExtent_ds.sel(time=slice('1980-01-01', '2014-01-01')).to_array()[1,:,0,0,0])
+plt.grid(True)
+plt.show()
+# }}} ==========================================================================
