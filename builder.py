1ca0279b082d68c045f803b7070495e9f2828712
Iain Russell <Iain.Russell@ecmwf.int>
2019-07-11 13:16:51 +0100

    INSTALLATION: allowed compiled installation [MPY-229]

---
+++ builder.py
@@ -0,0 +1,21 @@
+import logging
+
+import cffi
+import sys
+
+ffibuilder = cffi.FFI()
+ffibuilder.set_source(
+    "metview._bindings",
+    '#include <metview.h>',
+    libraries=["MvMacro"],
+)
+ffibuilder.cdef(
+    open("metview/metview.h").read()
+)
+
+if __name__ == "__main__":
+    try:
+        ffibuilder.compile(verbose=True)
+    except Exception:
+        logging.exception("can't compile Metview bindings")
+        sys.exit(1)
