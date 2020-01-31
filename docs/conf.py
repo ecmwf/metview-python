821c7a27c5e6b472d912c8840d6be61332d443dc
Iain Russell <Iain.Russell@ecmwf.int>
2020-01-31 14:39:08 +0000

    Update licence statement [MPY-251]

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# (C) Copyright 2017- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 import pkg_resources
 
 import sys
