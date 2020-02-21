7749149eff8415a6a1e880c443bba306ca38eb2f
Iain Russell <Iain.Russell@ecmwf.int>
2020-02-21 18:25:18 +0000

    FUNCTIONS: remove list of Macro functions as they are now automatically included [MPY-261]

--- metview/bindings.py
+++ metview/bindings.py
@@ -954,40 +954,6 @@ def bind_functions(namespace, module_name=None):
             namespace[python_name] = python_func
         # else:
         #    print('metview function %r not bound to python' % metview_name)
-    # add the 'mvl' functions, which are written in Macro and therefore not
-    # listed by the dictionary() function
-    for f in [
-        "mvl_ml2hPa",
-        "mvl_create_netcdf_2d",
-        "mvl_flextra_etadot",
-        "mvl_geocircle",
-        "mvl_geoline",
-        "mvl_geopotential_on_ml",
-        "mvl_mxn_subframes",
-        "mvl_plot_scm_data",
-        "mvl_regular_layout",
-        "mvl_regular_layout_area",
-        "thermo_data_info",
-        "thermo_parcel_path",
-        "thermo_parcel_area",
-        "xy_curve",
-        "potential_temperature",
-        "temperature_from_potential_temperature",
-        "saturation_mixing_ratio",
-        "mixing_ratio",
-        "relative_humidity_from_td",
-        "vapour_pressure",
-        "saturation_vapour_pressure",
-        "lifted_condensation_level",
-        "divergence",
-        "vorticity",
-        "laplacian",
-        "geostrophic_wind_pl",
-        "geostrophic_wind_ml",
-        "integral",
-        "ml_to_hl",
-    ]:
-        namespace[f] = make(f)
 
     # HACK: some fuctions are missing from the 'dictionary' call.
     namespace["neg"] = make("neg")
