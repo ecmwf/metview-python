c14632987a794e42eab340296ef9446a7c0f4ab1
Marco Di Bari <m.dibari@bopen.eu>
2020-05-15 15:40:43 +0200

    Update module header.

--- metview/modules.py
+++ metview/modules.py
@@ -1,13 +1,16 @@
-# (C) Copyright 2017- ECMWF.
+# Copyright 2020- European Centre for Medium-Range Weather Forecasts (ECMWF).
 #
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# In applying this licence, ECMWF does not waive the privileges and immunities
-# granted to it by virtue of its status as an intergovernmental organisation
-# nor does it submit to any jurisdiction.
-
-# requires a Python 3 interpreter
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import textwrap
