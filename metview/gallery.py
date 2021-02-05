fe57a5fb01fd276995e7ed4afacb6af0855acc45
Iain Russell <Iain.Russell@ecmwf.int>
2021-02-05 13:52:38 +0000

    DATA: renamed download_gallery_data to gallery.load_dataset [MPY-289]

---
+++ metview/gallery.py
@@ -0,0 +1,21 @@
+# (C) Copyright 2017- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import metview as mv
+
+
+def load_dataset(filename):
+    base_url = "http://download.ecmwf.org/test-data/metview/gallery/"
+    try:
+        d = mv.download(url=base_url + filename, target=filename)
+        return d
+    except:
+        raise Exception(
+            "Could not download file " + filename + " from the download server"
+        )
