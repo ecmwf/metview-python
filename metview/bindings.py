995ebf031fcd3b20208f252daaefb8600709db23
Iain Russell <Iain.Russell@ecmwf.int>
2022-02-28 15:35:55 +0000

    VERSION: updated version to 1.11.0

--- metview/bindings.py
+++ metview/bindings.py
@@ -35,7 +35,7 @@ from metview import plotting
 from metview.metviewpy.ipython import is_ipython_active, import_widgets
 from metview.metviewpy import utils
 
-__version__ = "1.10.4"
+__version__ = "1.11.0"
 
 
 # logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
